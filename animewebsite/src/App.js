7e5213cd07c1d489610206683f8be5b4615ffd88
arjun <arjunv2001@gmail.com>
2021-10-06 09:16:36 +0530

    made changes to the review form and added seasonal anime section

---
+++ animewebsite/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ReviewForm from "./components/Forms/animereviewform"
+
+function App() {
+  return (
+    <div className="App">
+        <ReviewForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ animewebsite/src/components/Background/review.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./review.css"
+
+function reviewBackground(){
+    return(
+        <div className="review">
+
+        </div>    
+    )
+}
+
+export default reviewBackground;
